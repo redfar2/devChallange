f48d84e7f1bbfe02f9e758ad7095966b166d7a58
redfar2 <red1farah@gmail.com>
2022-03-28 10:09:22 +0000

    Update avatar.js

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -5,7 +5,7 @@ function Avatar(props) {
     return (
 
         <div className="flex flex-col w-20 h-20 justify-center items-center mr-3">
-            <a href="#" className="block relative">
+            <button className="block relative">
                 <img alt="profil" src={avatar} className="mx-auto object-cover rounded-full h-20 w-20 " />
                 <span
                     className="absolute right-0  w-8 h-8 bg-regal-orange items-center border-2 border-white rounded-full color-white top-0 text-white"
@@ -14,7 +14,7 @@ function Avatar(props) {
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                     </svg>
                 </span>
-            </a>
+            </button>
         </div>
     )
 }
